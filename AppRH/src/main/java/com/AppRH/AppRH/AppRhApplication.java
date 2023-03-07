0a550272304c418ac7114295b23b93e5831117bf
Ruan fonseca <whitelook22@outlook.com>
2023-03-07 09:24:39 -0300

    primeiro commit

---
+++ AppRH/src/main/java/com/AppRH/AppRH/AppRhApplication.java
@@ -0,0 +1,15 @@
+package com.AppRH.AppRH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class AppRhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRhApplication.class, args);
+    }
+
+}
